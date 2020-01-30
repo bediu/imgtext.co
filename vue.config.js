f535a3d0a7dfb506fd66b0632151009f727e0841
bediu <fbediu@outlook.com>
2020-01-30 19:04:25 +0100

    added homepage

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  pwa: {
-    name: 'imgtext',
-    appleMobileWebAppStatusBarStyle: 'default'
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+
+  pages: {
+    app: {
+      entry: 'src/main.js',
+      template: 'public/app.html',
+      filename: 'app.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'app'],
+      pwa: {
+        name: 'imgtext',
+        appleMobileWebAppStatusBarStyle: 'default'
+      },
+    }
   }
 }
